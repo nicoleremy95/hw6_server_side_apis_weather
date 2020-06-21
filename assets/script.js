a9231ce6bd05cca506bea79d4bc02fb2b4ae383d
Nicole Remy <nicoleremy95@gmail.com>
2020-06-21 15:18:42 -0500

    submit click ready to go

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,26 @@
 
 
-var apiKey = "9fa65c8a19cf2131654f1622f89351d4";
 
-var cityName = $("input").text();
-
-var queryURL= "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid="  + apiKey;
- console.log(queryURL);
-
-//  $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-
-//     console.log(response);
-
-//     })
-
-
-
+//when the user submits the form 
 $(".searchBtn").click(function(event){
     event.preventDefault;
+    var apiKey = "9fa65c8a19cf2131654f1622f89351d4";
+    var cityNameInput = $("#city-input").val().trim();
+    var queryURL= "http://api.openweathermap.org/data/2.5/weather?q=" + cityNameInput + "&appid=" + apiKey + "&units=imperial"
+    // var weatherContainer = $("#weather-container");
+    console.log(queryURL);
+    
+    console.log('hi')
+   
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        
+        // var results = response.data; 
 
+        })
+       
 })
 
